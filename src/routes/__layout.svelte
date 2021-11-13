97692b021d861de30aad0fc3764659072df4b89e
Roger <rogelio.g.sandoval@gmail.com>
2021-11-13 14:26:32 -0600

    Initialize the project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="bg-blue-200 m-0 w-screen h-screen">
+	<slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+<section class="relataive font-sans pt-5">
+
+	<div class="text-center">
+		<p class="text-6xl">Sound-Box</p>
+	</div>
+
+	
+
+</section>
