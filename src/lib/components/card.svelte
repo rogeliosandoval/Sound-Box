f3873b32a645b63f9a7a458b15d87628d3eaefbe
Roger <rogelio.g.sandoval@gmail.com>
2021-11-14 13:12:54 -0600

    Create the cards

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,40 @@
+<script>
+
+    export let button
+
+    function playMyAudio(){
+       document.getElementById("myAudio").play();
+     }
+
+</script>
+
+<audio src="static/audio/toilet.wav" id="myAudio"></audio>
+
+<button onclick="playMyAudio"></button>
+
+<button onClick="playMyAudio()">
+    <div class="cards flex flex-col justify-center gap-x-2 max-w-2xl text-center">
+
+    <img class="mx-auto p-7" alt="{button.image}" src="{button.image}">
+
+    <div class="font-bold text-3xl pb-5">{button.name}</div>
+
+    </div>
+</button>
+
+
+<style>
+    img {
+        width: 250px;
+        height: 250px;
+    }
+
+    .cards{
+        border: 5px solid #000000;
+        background: lightgrey;
+    }
+
+    .cards:hover{
+        background-color: darkgrey;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,50 @@
 <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
 <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
 
-<section class="relataive font-sans pt-5">
+<script>
+
+	import Card from "$lib/components/card.svelte"
+
+	const buttons = [
+		{
+			name: "Train",
+			image: "../../img/train.png",
+			sound: "../../audio/train.wav"
+		},
+		{
+			name: "Golfer",
+			image: "../../img/golf.png",
+			sound: "../../audio/golf.wav"
+		},
+		{
+			name: "Bell",
+			image: "../../img/bell.png",
+			sound: "../../audio/bell.wav"
+		},
+		{
+			name: "Gun",
+			image: "../../img/gun.png",
+			sound: "../../audio/gun.wav"
+		},
+		{
+			name: "Toilet",
+			image: "../../img/toilet.png",
+			audio: "../../audio/toilet.wav"
+		}
+	]
+
+</script>
+
+<section class="relataive font-sans pt-5 bg-blue-200">
 
 	<div class="text-center">
 		<p class="text-6xl">Sound-Box</p>
 	</div>
 
+	<section class="mt-10 flex flex-wrap mx-auto w-3/4 gap-5 justify-center">
+		{#each buttons as button}
+			<Card button={button} />
+		{/each}
+	</section>
 	
-
 </section>
