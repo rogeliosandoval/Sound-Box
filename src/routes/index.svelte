210342c2796a54fc74efdafaa58a34e9b8bf8466
Roger <rogelio.g.sandoval@gmail.com>
2021-11-22 18:26:39 -0600

    Add edit button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,61 +1,140 @@
 <script>
+	import SoundFile from "$lib/components/soundFile.svelte"
 	import Modal from "$lib/components/modal.svelte";
 	import { onMount } from "svelte";
 
+
 	let media = [];
-	let mediaRecorder = null;
+	let soundClips = [];
+	let mediaRecorder;
 	let isOpenModal = false;
 
+	let audioCtx, canvas, canvasCtx;
+
+	//IF YOU USE DOCUMENT IT NEEDS TO BE INSIDE ONMOUNT
 	onMount(async () => {
+
 		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		canvas = document.querySelector('.visualizer');
+		canvasCtx = canvas.getContext('2d');
+		visualize(stream);
 
 		mediaRecorder = new MediaRecorder(stream);
 		mediaRecorder.ondataavailable = (e) => media.push(e.data);
 		mediaRecorder.onstop = function() {
-			const audio = document.querySelector('audio');
-			const blob = new Blob( media, { 'type' : 'audio/ogg; codecs=opus' });
+			const clipName = prompt('Enter a name for your sound clip','My Unnamed Clip');
+
+			const blob = new Blob( media, { 'type' : 'audio/wav; codecs=opus' });
 			media = [];
-			audio.src = window.URL.createObjectURL(blob);
+			const audioURL = window.URL.createObjectURL(blob);
+			const newSoundClip = {
+				label: (clipName === null) ? "Default" : clipName,
+				srcURL: audioURL,
+				id: crypto.randomUUID()
+			}
+			//spread y rest
+			soundClips = [...soundClips,newSoundClip];
 		}
-	})
+
+	});
+
+	function visualize(stream) {
+		if(!audioCtx) {
+			audioCtx = new AudioContext();
+		}
+
+		const source = audioCtx.createMediaStreamSource(stream);
+
+		const analyser = audioCtx.createAnalyser();
+		analyser.fftsize = 2048;
+		const bufferLength = analyser.frequencyBinCount;
+		const dataArray = new Uint8Array(bufferLength);
+
+		source.connect(analyser);
+
+		draw()
+
+		function draw() {
+			const WIDTH = canvas.width;
+			const HEIGHT = canvas.height;
+
+			requestAnimationFrame(draw);
+
+			analyser.getByteTimeDomainData(dataArray);
+
+			canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+			canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+			canvasCtx.lineWidth = 2;
+			canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+			canvasCtx.beginPath();
+
+			let sliceWidth = WIDTH * 1.0 / bufferLength;
+			let x = 0;
+
+			for (let i = 0; i < bufferLength; i++) {
+				let v = dataArray[i] / 128.0;
+				let y = v * HEIGHT/2;
+
+				if(i === 0) {
+					canvasCtx.moveTo(x, y);
+				} else {
+					canvasCtx.lineTo(x, y);
+				}
+
+				x += sliceWidth;
+			}
+
+			canvasCtx.lineTo(canvas.width, canvas.height/2);
+			canvasCtx.stroke();
+		}
+	}
 
 	function startRecording(){
+		isOpenModal = true;
 		mediaRecorder.start();
 	}
 
 	function stopRecording(){
+		isOpenModal = false;
 		mediaRecorder.stop();
 	}
 
-	function openModal() {
-        isOpenModal = true;
+	function deleteSoundClip(event){
+		soundClips = soundClips.filter(soundClip => soundClip.id != event.detail)
 	}
 
-    function closeModal() {
-        isOpenModal = false;
-    }
+	function editSoundClip(event){
+		const passedInId = event.detail;
+		console.log(passedInId)
+		const newClipName = prompt("Enter new clip name:");
+		soundClips = soundClips.map(soundClip => {
+			if(soundClip.id == passedInId){
+				soundClip.label = (newClipName === null) ? soundClip.label : newClipName;
+			}
+			return soundClip;
+		})
+	}
 
 </script>
 
 <section class="relative pb-12 pt-12 font-sans">
 
+	<canvas class="visualizer" height="60px"></canvas>
+
 	<div class="container mx-auto text-center">
-		<audio class="w-1/2 mx-auto"controls></audio>
-		<br>
-		<button on:click={openModal} class="bg-red-500 hover:bg-red-600" id="record" on:click={startRecording}>Record</button>
+		<button class="bg-red-500 hover:bg-red-600" id="record" on:click={startRecording}>Record</button>
 	</div>
 
-	<Modal {isOpenModal} on:closeModal={closeModal} on:closeModal={stopRecording} />
+	<Modal {isOpenModal} on:closeModal={stopRecording} /> 
 
-
+	<SoundFile {soundClips} on:deleteSoundClip={deleteSoundClip} on:editSoundClip={editSoundClip}/>
+	
 </section>
 
 
 <style>
-
-	audio::-webkit-media-controls-panel{
-		background: lightyellow;
-	}
 	
 	#record {
 		border: 2px solid #000000;
