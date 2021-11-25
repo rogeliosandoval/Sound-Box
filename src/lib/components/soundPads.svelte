90ca33025cac81a73ce345e2f48dc2b727b8dd9f
Roger <rogelio.g.sandoval@gmail.com>
2021-11-24 20:43:13 -0600

    Update the logic of soundPads

--- src/lib/components/soundPads.svelte
+++ src/lib/components/soundPads.svelte
@@ -4,50 +4,82 @@
 
 	import Card from "$lib/components/card.svelte"
 
-	const buttons = [
+	let buttons = [
 		{
 			name: "Train",
 			image: "img/train.png",
 			sound: "audio/train.wav",
-            key: "1"
+            key: "a",
+            isClicked: false
 		},
 		{
 			name: "Golfer",
 			image: "img/golf.png",
 			sound: "audio/golf.wav",
-            key: "2"
+            key: "s",
+            isClicked: false
 		},
 		{
 			name: "Bell",
 			image: "img/bell.png",
 			sound: "audio/bell.wav",
-            key: "3"
+            key: "d",
+            isClicked: false
 		},
 		{
 			name: "Gun",
 			image: "img/gun.png",
 			sound: "audio/gun.wav",
-            key: "4"
+            key: "f",
+            isClicked: false
 		},
 		{
 			name: "Toilet",
 			image: "img/toilet.png",
 			sound: "audio/toilet.wav",
-            key: "5"
+            key: "g",
+            isClicked: false
 		}
 	]
 
-    onMount(() => document.addEventListener('keydown', (event) => playMyAudio(event.key)))
+	onMount(() => {
+		document.addEventListener('keydown', (event) => {
+			playMyAudio(event.key)
+		})
+		document.addEventListener('keyup', (event) => {
+			setTimeout(() =>{
+				buttons = buttons.map(btn => {
+					btn.isClicked = false;
+					return btn;
+				})
+				stopMyAudio(event.key)
+			}, 200)
+		})
+	})
+
 	function playMyAudio(keyCode){
         const button = buttons.filter(btn => btn.key == keyCode).shift();
-		let myAudio = new Audio(button.sound);
-        console.log(keyCode)
-        myAudio.play();
+		if(button){
+			let myAudio = new Audio(button.sound);
+			myAudio.play();
+		}
+		buttons = buttons.map(btn => {
+			if(btn.key === keyCode) btn.isClicked = true;
+			return btn;
+		})
     }
+
+    function stopMyAudio(keyCode){
+		const button = buttons.filter(btn => btn.key == keyCode).shift();
+		if(button){
+			let myAudio = new Audio(button.sound);
+			myAudio.pause();
+		}
+	}
 
 </script>
 
-<section class="relataive font-sans pt-5 bg-blue-200">
+<section class="font-sans pt-5">
 
 	<section class="mt-10 flex flex-wrap mx-auto w-3/4 gap-5 justify-center">
 		{#each buttons as button}
