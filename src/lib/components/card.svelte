63c5397590f529fa94183d24a839688af591510f
Roger <rogelio.g.sandoval@gmail.com>
2021-11-25 22:49:42 -0600

    Display the keys on each card

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -9,12 +9,13 @@
 
 </script>
 
-<button on:mousedown={playMyAudio(button.sound)} class="{button.isClicked ? "transition duration-100 ease-in-out transform translate-y-5 scale-140" : ""}">
+<button on:mousedown={playMyAudio(button.sound)} class="{button.isClicked ? "transition duration-100 ease-in-out transform translate-y-4 scale-140" : ""}">
     <div class="cards flex flex-col justify-center gap-x-2 max-w-2xl text-center">
 
+    <div class="font-bold text-3xl pt-3">{button.name}</div>
     <img class="mx-auto p-7" alt="{button.image}" src="{button.image}">
+    <div class="font-bold text-xl pb-3">{button.key}</div>
 
-    <div class="font-bold text-3xl pb-5">{button.name}</div>
 
     </div>
 </button>
@@ -22,7 +23,7 @@
 <style>
     img {
         width: 250px;
-        height: 250px;
+        height: 240px;
     }
 
     .cards{
