176308e48c81f9c0950583ffc42ab7c54c038369
Roger <rogelio.g.sandoval@gmail.com>
2021-12-01 15:50:40 -0600

    Create new loop cards component

---
+++ src/lib/components/cards/loopCards.svelte
@@ -0,0 +1,59 @@
+<script>
+
+    export let button
+    let myAudio;
+    let isActive = false;
+
+    function playMyAudio(button){
+        if(!button.isClicked){
+            myAudio = new Audio(button.sound);
+            myAudio.play();
+            myAudio.loop = true;
+            button.isClicked = true;
+        } else {
+            myAudio.pause();
+            myAudio.currentTime = 0;
+            button.isClicked = false;
+        }
+        isActive = !isActive
+    }
+
+</script>
+
+<button on:mousedown={playMyAudio(button)} class="{button.isClicked ? "transition duration-100 ease-in-out transform translate-y-3 translate-x-3 scale-140" : ""}">
+    <div class="{button.isClicked ? "bg-green-600 box" : ""} bg-green-300 cards flex flex-col justify-center gap-x-2 max-w-2xl text-center box" class:active={isActive}>
+
+    <div class="font-bold text-3xl pt-3">{button.name}</div>
+    <img class="mx-auto p-7" alt="{button.image}" src="{button.image}">
+    <div class="font-bold text-xl pb-3">{button.key}</div>
+
+
+    </div>
+</button>
+
+<style>
+    .box{
+        box-shadow: 5px 5px 5px #000000;
+    }
+
+    .active {
+		box-shadow: none;
+        transform: translate(10px, 10px);
+        background: teal;
+	}
+
+    img {
+        width: 250px;
+        height: 240px;
+    }
+
+    .cards{
+        border: 5px solid darkblue;
+        transition: background-color 0.25s linear
+    }
+
+    .cards:hover{
+        background-color: teal;
+    }
+
+</style>
