f26eafb6a8e4607edb2abf59b4eba9db0f70810b
Roger <rogelio.g.sandoval@gmail.com>
2022-08-08 14:31:15 -0500

    Update project

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import netlify from '@sveltejs/adapter-netlify'
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: netlify()
+		adapter: netlify(
+			{
+				edge:false,
+				split:false
+			}
+		)
 	},
 
 	preprocess: [
